3971a6b9675bbf614fb47792a9bc8d78e1271cea
KaanOzen-mF <kaanozn98@gmail.com>
2025-12-22 17:52:24 +0300

    refactor: remove quickSort import and use sortFunction parameter in performance measurement

--- sorting-analysis/src/utils/performanceTracker.js
+++ sorting-analysis/src/utils/performanceTracker.js
@@ -3,8 +3,6 @@
  * Measures execution time and memory usage of the provided sorting function.
  */
 
-import { quickSort } from "../algorithms/quickSort";
-
 export const measurePerformance = async (sortFunction, array) => {
   // Create a copy of the original array to avoid modifying it (Important for analyzing In-place vs Additional memory)
   const arrayCopy = [...array];
@@ -18,7 +16,7 @@ export const measurePerformance = async (sortFunction, array) => {
     : 0;
 
   // Run the algorithm
-  await quickSort(arrayCopy, 0, arrayCopy.length - 1, null, 0);
+  await sortFunction(arrayCopy, 0, arrayCopy.length - 1, null, 0);
 
   // Get end values
   const endTime = performance.now();
